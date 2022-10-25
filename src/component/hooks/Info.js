bd985f7a72a399dcc16656c89bcd0c588f83df4f
김민혜 <kmhkgh@naver.com>
2022-10-25 11:08:34 +0900

    Hooks tutorial: useEffect

--- src/component/hooks/Info.js
+++ src/component/hooks/Info.js
@@ -1,4 +1,4 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
 
 const Info = () => {
   const [name, setName] = useState('');
@@ -11,6 +11,42 @@ const Info = () => {
   const onChangeNickname = e => {
     setNickname(e.target.value);
   };
+
+  //배열 값 자체를 받지 않으면 컴포넌트가 렌더링 될 때 마다 호출
+  // useEffect(() => {
+  //   console.log('렌더링이 완료되었습니다!');
+  //   console.log({ name, nickname });
+  // });
+
+  // // 컴포넌트가 화면에 맨 처음 렌더링 될 때만 호츌되고, 업데이트될 때는 호출되지 않음
+  // useEffect(() => {
+  //   console.log('마운트 될 때만 실행됩니다.');
+  // }, []);
+
+  // // name의 상태값이 변경 될 때 마다 호출
+  // useEffect(() => {
+  //   console.log(name);
+  // }, [name]);
+
+  //뒷정리 함수
+  // useEffect(() => {
+  //   console.log('effect');
+  //   console.log(name);
+
+  //   return () => {
+  //     console.log('cleanup');
+  //     console.log(name);
+  //   };
+  // }, [name]);
+
+  //언마운트될 때만 뒷정리 함수를 호출
+  useEffect(() => {
+    console.log('effect');
+
+    return () => {
+      console.log('unmout');
+    };
+  }, []);
 
   return (
     <>
--- src/pages/HookIndex.js
+++ src/pages/HookIndex.js
@@ -1,11 +1,23 @@
+import { useState } from 'react';
 import Counter from '../component/hooks/Counter';
 import Info from '../component/hooks/Info';
 
 const HookIndex = () => {
+  const [visible, setVisible] = useState(false); // info 컴포넌트의 가시성을 바꿈
   return (
     <>
       <Counter /> <br />
-      <Info />
+      <div>
+        <button
+          onClick={() => {
+            setVisible(!visible);
+          }}
+        >
+          {visible ? '숨기기' : '보이기'}
+        </button>
+      </div>
+      {visible && <Info />}{' '}
+      {/* 컴포넌트가 나타날때 effect가 나타나고, 사라질 때 cleanup이 나타남 */}
     </>
   );
 };
